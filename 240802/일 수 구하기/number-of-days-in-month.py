9e5b6cb8c3c1b07a8cfbb22b5dcae164a14a49e8
KimChaeRin-0601 <kcl89500131@gmail.com>
2024-08-02 16:01:04 +0900

    Title: 일 수 구하기, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/일 수 구하기/number-of-days-in-month.py
+++ 240802/일 수 구하기/number-of-days-in-month.py
@@ -1,8 +1,16 @@
-a = int(input())
-if (a == 1) or (a==3) or (a==5) or(a==7) or (a==8) or(a==10) (a==12):
-    print("31")
+# 변수 선언, 입력
+n = int(input())
+
+# 출력
+if n == 2:
+	print("28")
+elif n <= 7:
+    if n % 2 == 1:
+        print("31")
+    else:
+        print("30")
 else:
-    if a ==2:
-        print("28")
+    if n % 2 == 0:
+        print("31")
     else:
         print("30")
