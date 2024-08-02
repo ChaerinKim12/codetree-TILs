461e81a48e9241a1d15c18b473a4fb64c4dc0d30
KimChaeRin-0601 <kcl89500131@gmail.com>
2024-08-02 15:22:32 +0900

    Title: 특정 조건 세 정수 비교, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240802/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 240802/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -1,20 +1,16 @@
+# 세 개의 정수를 입력받습니다.
 a, b, c = map(int, input().split())
+
+# 가장 작은 수를 찾고, 그 수가 a와 같은지 비교합니다.
 if a < b and a < c:
-    if a == a:
-        print("1", end=" ")
-    else:
-        print("0", end=" ")
+    print("1", end=" ")
 elif b < a and b < c:
-    if a == b:
-        print("1", end=" ")
-    else:
-        print("0", end=" ")
+    print("0", end=" ")
 elif c < a and c < b:
-    if a == c:
-        print("1", end=" ")
-    else:
-        print("0", end=" ")
-if a == b and a == c and b == c:
+    print("0", end=" ")
+
+# 세 수가 모두 같은지 확인합니다.
+if a == b and a == c:
     print("1")
 else:
     print("0")
