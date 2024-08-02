e28d0fcb07ca4cf8bf60216cb780fedd45fefaa0
KimChaeRin-0601 <kcl89500131@gmail.com>
2024-08-02 16:19:09 +0900

    Title: 중앙값 구하기, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/중앙값 구하기/find-the-median.py
+++ 240802/중앙값 구하기/find-the-median.py
@@ -1,7 +1,8 @@
 a, b, c = map(int, input().split())
-if a < b  and b < c:
+
+if (a <= b <= c) or (c <= b <= a):
     print(b)
-elif b < a and a < c:
+elif (b <= a <= c) or (c <= a <= b):
     print(a)
 else:
     print(c)
