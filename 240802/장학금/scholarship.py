c48a2a6e6855ef1106c008455e12f99a23b71d23
KimChaeRin-0601 <kcl89500131@gmail.com>
2024-08-02 15:36:38 +0900

    Title: 장학금, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240802/장학금/scholarship.py
@@ -0,0 +1,10 @@
+a, b = map(int, input().split())
+if a >= 90:
+    if b >= 95:
+        print("100000")
+    elif 95 > b >= 90:
+        print("50000")
+    else:
+        print("0")
+else:
+    print("0")
