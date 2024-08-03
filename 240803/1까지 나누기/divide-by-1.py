0bddf5ef390ea22373c847c35cda8e1c41cbe95d
KimChaeRin-0601 <kcl89500131@gmail.com>
2024-08-03 20:16:40 +0900

    Title: 1까지 나누기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240803/1까지 나누기/divide-by-1.py
+++ 240803/1까지 나누기/divide-by-1.py
@@ -1,13 +1,11 @@
-def division_steps(n):
-    count = 0
-    divisor = 1
-    while n > 1:
-        divisor += 1
-        n = n // divisor
-        count += 1
-    return count
+n = int(input())
 
-# 입력 받기
-n = int(input().strip())
-# 결과 출력
-print(division_steps(n))
+cnt = 0
+
+for i in range(1, n+1):
+    n //= i
+    cnt += 1
+    if n <= 1:
+        break
+
+print(cnt)
