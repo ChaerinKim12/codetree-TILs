f4e862baabaae9a38e70fe128ffd33840eb8d65b
KimChaeRin-0601 <kcl89500131@gmail.com>
2024-08-03 22:50:25 +0900

    Title: 3n + 1 수열, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240803/3n + 1 수열/3n-plus-1-sequence.py
+++ 240803/3n + 1 수열/3n-plus-1-sequence.py
@@ -1,12 +1,13 @@
 cnt = 0
 n= int(input())
 while True:
+    if n == 1:
+        break
     if n % 2 == 0:
         n = n// 2
         
     else:
         n = (n*3) + 1
     cnt += 1
-    if n == 1:
-        break
+
 print(cnt)
