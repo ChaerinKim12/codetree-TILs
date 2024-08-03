51bcd1a6cba9258d66fd3944e754823fd8ea8c9d
KimChaeRin-0601 <kcl89500131@gmail.com>
2024-08-03 22:50:04 +0900

    Title: 3n + 1 수열, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240803/3n + 1 수열/3n-plus-1-sequence.py
+++ 240803/3n + 1 수열/3n-plus-1-sequence.py
@@ -2,11 +2,11 @@ cnt = 0
 n= int(input())
 while True:
     if n % 2 == 0:
-        n /= 2
-        cnt += 1
+        n = n// 2
+        
     else:
         n = (n*3) + 1
-        cnt += 1
+    cnt += 1
     if n == 1:
         break
 print(cnt)
