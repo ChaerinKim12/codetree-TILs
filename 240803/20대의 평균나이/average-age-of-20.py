a1f4ea35668f5adce220697c207ed5288dd93d46
KimChaeRin-0601 <kcl89500131@gmail.com>
2024-08-03 22:44:30 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240803/20대의 평균나이/average-age-of-20.py
+++ 240803/20대의 평균나이/average-age-of-20.py
@@ -2,10 +2,9 @@ hap = 0
 cnt = 0
 while True:
     age = int(input())
-   
     if age >= 30:
         break
-        hap += age
-        cnt += 1
+    hap += age
+    cnt += 1
 avg = hap / cnt
 print(f"{avg:.2f}")
