acd56e20a942052c380bb4ad92cc623d8d3a244b
KimChaeRin-0601 <kcl89500131@gmail.com>
2024-10-09 11:20:05 +0900

    Title: 좀 더 어려운 수학 점수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241009/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a_math, a_eng, b_math, b_eng;
+    cin >> a_math >> a_eng;
+    cin >> b_math >> b_eng;
+    if (a_math>b_math)
+    cout << "A";
+    else if ((a_math==b_math) && (a_eng>b_eng))
+    cout << "A";
+    else if ((a_math==b_math) && (a_eng<b_eng))
+    cout << "B";
+    else
+    cout << "B";
+    return 0;
+}
