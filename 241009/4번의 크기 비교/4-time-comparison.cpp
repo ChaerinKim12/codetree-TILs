b0f86e6e1d3d659b511099fbc7516035e227b0b7
KimChaeRin-0601 <kcl89500131@gmail.com>
2024-10-09 10:51:17 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241009/4번의 크기 비교/4-time-comparison.cpp
+++ 241009/4번의 크기 비교/4-time-comparison.cpp
@@ -5,7 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, c, d, e;
     cin >> a;
-    cin >> b, c, d, e;
+    cin >> b >> c >> d >> e;
     cout << (a>b) <<endl;
     cout << (a>c) << endl;
     cout << (a>d) << endl;
